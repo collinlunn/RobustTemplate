c31fac061c225c0668059f5d5e0e5428a300ca80
CL <60152240+collinlunn@users.noreply.github.com>
2023-02-20 20:21:15 -0800

    Adds base LobbyHud

---
+++ Content.Client/UserInterface/States/LobbyState.cs
@@ -0,0 +1,28 @@
+using Content.Client.UserInterface.Hud;
+using Robust.Client.State;
+using Robust.Client.UserInterface;
+using Robust.Client.UserInterface.Controls;
+using Robust.Shared.IoC;
+
+namespace Content.Client.UserInterface.States;
+
+public sealed class LobbyState : State
+{
+    [Dependency] private readonly IUserInterfaceManager _userInterface = default!;
+        
+    private LobbyHud? _lobby;
+        
+    protected override void Startup()
+    {
+        _lobby = new LobbyHud();
+
+        LayoutContainer.SetAnchorAndMarginPreset(_lobby, LayoutContainer.LayoutPreset.Wide);
+            
+        _userInterface.StateRoot.AddChild(_lobby);
+    }
+        
+    protected override void Shutdown()
+    {
+        _lobby?.Dispose();
+    }
+}
