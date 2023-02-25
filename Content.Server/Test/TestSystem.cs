2f170a3eba5c81faee9a94fa82a643a143e4e432
CL <60152240+collinlunn@users.noreply.github.com>
2023-02-24 22:05:12 -0800

    TestComponent

---
+++ Content.Server/Test/TestSystem.cs
@@ -0,0 +1,22 @@
+﻿using Content.Shared.Test;
+using JetBrains.Annotations;
+using Robust.Shared.GameObjects;
+using Robust.Shared.Maths;
+
+namespace Content.Server.Test
+{
+    [UsedImplicitly]
+    public sealed class TestSystem : SharedTestSystem
+    {
+        public override void Update(float frameTime)
+        {
+            base.Update(frameTime);
+
+            var testComponents = EntityQuery<TestComponent>();
+            foreach (var comp in testComponents)
+            {
+                //TEST
+            }
+        }
+    }
+}
---
+++ Content.Shared/Test/SharedTestSystem.cs
@@ -0,0 +1,20 @@
+﻿using JetBrains.Annotations;
+using Robust.Shared.GameObjects;
+
+namespace Content.Shared.Test
+{
+    [UsedImplicitly]
+    public abstract class SharedTestSystem : EntitySystem
+    {
+        public override void Update(float frameTime)
+        {
+            base.Update(frameTime);
+
+            var testComponents = EntityQuery<TestComponent>();
+            foreach (var comp in testComponents)
+            {
+                //TEST
+            }
+        }
+    }
+}
