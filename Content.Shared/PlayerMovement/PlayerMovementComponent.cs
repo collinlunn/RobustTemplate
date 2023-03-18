cd2385df1116fb77d31dbca1a0ee25ed43088a06
CL <60152240+collinlunn@users.noreply.github.com>
2023-03-18 15:50:40 -0700

    Functioning player movement controls

---
+++ Content.Shared/PlayerMovement/PlayerMovementComponent.cs
@@ -0,0 +1,18 @@
+﻿using Robust.Shared.GameObjects;
+using Robust.Shared.Serialization.Manager.Attributes;
+using Robust.Shared.Timing;
+
+namespace Content.Shared.PlayerMovement
+{
+	[RegisterComponent]
+	public sealed class PlayerMovementComponent : Component
+	{
+		public MoveButtons HeldButtons = MoveButtons.None;
+
+		public GameTick LastInputTick;
+		public ushort LastInputSubTick;
+
+		[DataField("speed")]
+		public ushort Speed = 1;
+	}
+}
--- Content.Shared/PlayerMovement/SharedPlayerMovementController.cs
+++ Content.Shared/PlayerMovement/SharedPlayerMovementController.cs
@@ -6,21 +6,25 @@ using Robust.Shared.GameObjects;
 using Robust.Shared.Physics.Systems;
 using Robust.Shared.IoC;
 using Robust.Shared.Maths;
+using System;
+using Robust.Shared.Timing;
+using Robust.Shared.Physics.Components;
 
 namespace Content.Shared.PlayerMovement
 {
     public class SharedPlayerMovementController : VirtualController
     {
         [Dependency] protected readonly SharedPhysicsSystem _physics = default!;
+		[Dependency] protected readonly IGameTiming _timing = default!;
 
-        public override void Initialize()
+		public override void Initialize()
         {
             base.Initialize();
 
-            var upHandler = new PlayerMovementInputCmdHandler(this, Direction.North);
-			var downHandler = new PlayerMovementInputCmdHandler(this, Direction.South);
-			var leftHandler = new PlayerMovementInputCmdHandler(this, Direction.West);
-			var rightHandler = new PlayerMovementInputCmdHandler(this, Direction.East);
+            var upHandler = new PlayerMovementInputCmdHandler(this, MoveButtons.Up);
+			var downHandler = new PlayerMovementInputCmdHandler(this, MoveButtons.Down);
+			var leftHandler = new PlayerMovementInputCmdHandler(this, MoveButtons.Left);
+			var rightHandler = new PlayerMovementInputCmdHandler(this, MoveButtons.Right);
 
 			CommandBinds.Builder
                 .Bind(EngineKeyFunctions.MoveUp, upHandler)
@@ -36,32 +40,72 @@ namespace Content.Shared.PlayerMovement
 			CommandBinds.Unregister<SharedPlayerMovementController>();
 		}
         
-        private void HandleMovementInput(EntityUid entity, ushort subTick, bool buttonPressed)
+        private void HandleMovementInput(EntityUid entity, ushort subTick, MoveButtons button, bool buttonPressed)
         {
+			var playerMovement = entity.EnsureComponentWarn<PlayerMovementComponent>();
 
-        }
+			if (buttonPressed)
+				playerMovement.HeldButtons |= button;
+			else
+				playerMovement.HeldButtons &= ~button;
+			
+			playerMovement.LastInputTick = _timing.CurTick;
+			playerMovement.LastInputSubTick = subTick;
+			var newVelocity = ButtonsToVelocityDir(playerMovement.HeldButtons).Normalized * playerMovement.Speed;
+			
+			PhysicsSystem.SetLinearVelocity(entity, newVelocity);
+		}
 
-        private sealed class PlayerMovementInputCmdHandler : InputCmdHandler
+		private sealed class PlayerMovementInputCmdHandler : InputCmdHandler
         {
             private readonly SharedPlayerMovementController _controller;
-			private readonly Direction _dir;
+			private readonly MoveButtons _button;
 
-			public PlayerMovementInputCmdHandler(SharedPlayerMovementController controller, Direction dir)
+			public PlayerMovementInputCmdHandler(SharedPlayerMovementController controller, MoveButtons button)
             {
                 _controller = controller;
-				_dir = dir;
+				_button = button;
 
 			}
 
-            //what does this do why do we return false here
+            //why are we returning false here
             public override bool HandleCmdMessage(ICommonSession session, InputCmdMessage message)
             {
                 if (message is not FullInputCmdMessage full || session?.AttachedEntity == null) return false;
 
                 var buttonPressed = full.State == BoundKeyState.Down;
-                _controller.HandleMovementInput(session.AttachedEntity.Value, message.SubTick, buttonPressed);
+                _controller.HandleMovementInput(session.AttachedEntity.Value, message.SubTick, _button, buttonPressed);
                 return false;
             }
         }
+
+		private static Vector2 ButtonsToVelocityDir(MoveButtons buttons)
+		{
+			var velocity = new Vector2(0, 0);
+
+			if (buttons.HasFlag(MoveButtons.Up))
+				velocity += new Vector2(0, 1);
+
+			if (buttons.HasFlag(MoveButtons.Down))
+				velocity += new Vector2(0, -1);
+
+			if (buttons.HasFlag(MoveButtons.Left))
+				velocity += new Vector2(-1, 0);
+
+			if (buttons.HasFlag(MoveButtons.Right))
+				velocity += new Vector2(1, 0);
+
+			return velocity;
+		}
     }
+
+	[Flags]
+	public enum MoveButtons : byte
+	{
+		None = 0,
+		Up = 1 << 0,
+		Down = 1 << 1,
+		Left = 1 << 2,
+		Right = 1 << 3,
+	}
 }
