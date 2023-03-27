f1cf7e87d2110df6dceff738a5705475205993dc
CL <60152240+collinlunn@users.noreply.github.com>
2023-03-26 23:17:59 -0700

    Moves LobbyHud to a UIController

--- Content.Client/Lobby/LobbyState.cs
+++ Content.Client/Lobby/LobbyState.cs
@@ -1,48 +1,16 @@
-using Content.Shared.Lobby;
-using Robust.Client;
 using Robust.Client.State;
-using Robust.Client.UserInterface;
-using Robust.Client.UserInterface.Controls;
-using Robust.Shared.GameObjects;
-using Robust.Shared.IoC;
-using Robust.Shared.Network;
 
 namespace Content.Client.Lobby;
 
 public sealed class LobbyState : State
 {
-    [Dependency] private readonly IUserInterfaceManager _userInterface = default!;
-    [Dependency] private readonly IEntityNetworkManager _entityNetManager = default!;
-	[Dependency] private readonly IClientNetManager _clientNetManager = default!;
-	[Dependency] private readonly IGameController _gameController = default!;
+	protected override void Startup()
+	{
+		
+	}
 
-	private LobbyHud? _lobbyHud;
-
-    protected override void Startup()
-    {
-        _lobbyHud = new LobbyHud();
-        _lobbyHud.StartGameButton.OnPressed += _ => StartGamePressed();
-
-        LayoutContainer.SetAnchorAndMarginPreset(_lobbyHud, LayoutContainer.LayoutPreset.Wide);
-		_lobbyHud.DisconnectButton.OnPressed += _ =>
-		{
-			_clientNetManager.ClientDisconnect("Client pressed disconnect button.");
-		};
-		_lobbyHud.QuitButton.OnPressed += _ =>
-		{
-			_gameController.Shutdown("Client pressed quit button.");
-		};
-
-		_userInterface.StateRoot.AddChild(_lobbyHud);
-    }
-
-    private void StartGamePressed()
-    {
-        _entityNetManager.SendSystemNetworkMessage(new StartGamePressedEvent());
-    }
-
-    protected override void Shutdown()
-    {
-        _lobbyHud?.Dispose();
-    }
+	protected override void Shutdown()
+	{
+		
+	}
 }
