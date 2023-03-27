6e8b7693d321effe89c0b36f44e38f3590d4efc1
CL <60152240+collinlunn@users.noreply.github.com>
2023-03-26 21:44:13 -0700

    EscapeMenu Disconnect, Quit button

--- Content.Client/EscapeMenu/EscapeMenuController.cs
+++ Content.Client/EscapeMenu/EscapeMenuController.cs
@@ -1,24 +1,50 @@
 using Content.Client.InGame;
+using Content.Client.MainMenu;
 using JetBrains.Annotations;
-using Robust.Client.Input;
+using Robust.Client;
+using Robust.Client.State;
 using Robust.Client.UserInterface.Controllers;
 using Robust.Shared.Input;
 using Robust.Shared.Input.Binding;
+using Robust.Shared.IoC;
+using Robust.Shared.Network;
 using Robust.Shared.Utility;
+using System;
 
 namespace Content.Client.EscapeMenu;
 
 [UsedImplicitly]
 public sealed class EscapeMenuController : UIController, IOnStateEntered<InGameState>, IOnStateExited<InGameState>
 {
+	[Dependency] private readonly IGameController _gameController = default!;
+	[Dependency] private readonly IClientNetManager _netManager = default!;
+	[Dependency] private readonly IStateManager _stateManager = default!;
+
 	private EscapeMenu? _escapeWindow;
 
 	public void OnStateEntered(InGameState state)
     {
         DebugTools.Assert(_escapeWindow == null);
         _escapeWindow = UIManager.CreateWindow<EscapeMenu>();
+		_escapeWindow.Title = "Escape Menu";
 
-        CommandBinds.Builder
+		_escapeWindow.OptionsButton.OnPressed += _ =>
+		{
+			throw new NotImplementedException(); //TODO Make options menu
+		};
+		_escapeWindow.DisconnectButton.OnPressed += _ =>
+		{
+			CloseWindow();
+			_netManager.ClientDisconnect("Client pressed disconnect button.");
+			_stateManager.RequestStateChange<MainMenuState>();
+		};
+		_escapeWindow.QuitButton.OnPressed += _ =>
+		{
+			CloseWindow();
+			_gameController.Shutdown("Client pressed quit button.");
+		};
+
+		CommandBinds.Builder
             .Bind(EngineKeyFunctions.EscapeMenu,
                 InputCmdHandler.FromDelegate(_ => ToggleWindow()))
             .Register<EscapeMenuController>();
