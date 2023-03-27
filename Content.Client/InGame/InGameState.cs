2935100f1a9876af4a3121146272ce2dcbc05917
CL <60152240+collinlunn@users.noreply.github.com>
2023-03-26 22:41:05 -0700

    Fixes keybinds sticking when switching to UI focus

--- Content.Client/InGame/InGameState.cs
+++ Content.Client/InGame/InGameState.cs
@@ -2,6 +2,7 @@ using Robust.Client.GameObjects;
 using Robust.Client.Input;
 using Robust.Client.Player;
 using Robust.Client.State;
+using Robust.Client.UserInterface;
 using Robust.Client.UserInterface.CustomControls;
 using Robust.Shared.GameObjects;
 using Robust.Shared.Input;
@@ -19,8 +20,9 @@ public sealed class InGameState : State
     [Dependency] private readonly IEntitySystemManager _entitySystemManager = default!;
     [Dependency] private readonly IGameTiming _timing = default!;
     [Dependency] private readonly IMapManager _mapManager = default!;
+	[Dependency] private readonly IUserInterfaceManager _uiManager = default!;
 
-    protected override void Startup()
+	protected override void Startup()
     {
         _inputManager.KeyBindStateChanged += OnKeyBindStateChanged;
     }
@@ -36,39 +38,49 @@ public sealed class InGameState : State
 	/// <param name="args">Details what changed.</param>
     private void OnKeyBindStateChanged(ViewportBoundKeyEventArgs args)
     {
-        // If there is no InputSystem, then there is nothing to forward to, and nothing to do here.
-        if (!_entitySystemManager.TryGetEntitySystem(out InputSystem? inputSys))
-            return;
+		if (args.Viewport != null)
+		{
+			HandleInput(args);
+		}
+		else
+		{
+			//var viewPort = _uiManager.ActiveScreen!.GetWidget<>()!; 
+			//var newArgs = new ViewportBoundKeyEventArgs(args.KeyEventArgs, viewPort)
+			//HandleInput(newArgs);
+			HandleInput(args); //TODO: Look at why we'd want to sub in new viewport
+		}
+	}
 
-		//When window is focused on top bar or a UI, substitute an appropriate ViewPort
-		if (args.Viewport == null)
-            return; //_uiManager.ActiveScreen!.GetWidget<>(MainViewport)!;
+	private void HandleInput(ViewportBoundKeyEventArgs args)
+	{
+		if (!_entitySystemManager.TryGetEntitySystem(out InputSystem? inputSys))
+			return;
 
 		var keyEventArgs = args.KeyEventArgs;
-        var function = keyEventArgs.Function;
-        var functionID = _inputManager.NetworkBindMap.KeyFunctionID(function);
+		var function = keyEventArgs.Function;
+		var functionID = _inputManager.NetworkBindMap.KeyFunctionID(function);
 
-        EntityCoordinates coordinates = default;
-        EntityUid entityToClick = default;
-        if (args.Viewport is IViewportControl vp)
-        {
+		EntityCoordinates coordinates = default;
+		EntityUid entityToClick = default;
+		if (args.Viewport is IViewportControl vp)
+		{
 			//Get entity under mouse when keybind state changed
 			var mousePosWorld = vp.ScreenToMap(keyEventArgs.PointerLocation.Position);
 			if (TryGetClickedEntity(mousePosWorld, out var foundEntity))
-				entityToClick = (EntityUid) foundEntity;
+				entityToClick = (EntityUid)foundEntity;
 
 			//Get coordinates of mouse when keybind state changed
-			coordinates = _mapManager.TryFindGridAt(mousePosWorld, out var grid) ? 
+			coordinates = _mapManager.TryFindGridAt(mousePosWorld, out var grid) ?
 				grid.MapToGrid(mousePosWorld) :
-                EntityCoordinates.FromMap(_mapManager, mousePosWorld);
-        }
+				EntityCoordinates.FromMap(_mapManager, mousePosWorld);
+		}
 
-        var message = new FullInputCmdMessage(_timing.CurTick, _timing.TickFraction, functionID, keyEventArgs.State,
-            coordinates, keyEventArgs.PointerLocation, entityToClick);
+		var message = new FullInputCmdMessage(_timing.CurTick, _timing.TickFraction, functionID, keyEventArgs.State,
+			coordinates, keyEventArgs.PointerLocation, entityToClick);
 
-        // client side command handlers will always be sent the local player session.
-        var session = _playerManager.LocalPlayer?.Session;
-        if (inputSys.HandleInputCommand(session, function, message))
+		// client side command handlers will always be sent the local player session.
+		var session = _playerManager.LocalPlayer?.Session;
+		if (inputSys.HandleInputCommand(session, function, message))
 			keyEventArgs.Handle();
 	}
 
