c48947d1bee50719aaaa115dc354cc637ac19442
CL <60152240+collinlunn@users.noreply.github.com>
2023-03-27 22:27:06 -0700

    LobbyHudState

---
+++ Content.Shared/Lobby/SharedLobbyHud.cs
@@ -0,0 +1,64 @@
+﻿using Robust.Shared.GameObjects;
+using Robust.Shared.Serialization;
+using System;
+using System.Collections.Generic;
+
+namespace Content.Shared.Lobby
+{
+	/// <summary>
+	///		Transmits the state of the lobby  UI.
+	/// </summary>
+	[Serializable, NetSerializable]
+	public sealed class LobbyUIStateEvent : EntityEventArgs
+	{
+		/// <summary>
+		///		Data on each player in the lobby.
+		/// </summary>
+		public IEnumerable<LobbyPlayerState> PlayersInLobby { get; }
+	}
+
+	/// <summary>
+	///		Data about a aprticular player in the lobby.
+	/// </summary>
+	[Serializable, NetSerializable]
+	public sealed class LobbyPlayerState
+	{
+		/// <summary>
+		///  Name of the player.
+		/// </summary>
+		public string Name { get; }
+
+		/// <summary>
+		///		If this player is ready for the game to start.
+		/// </summary>
+		public bool Ready { get; }
+	}
+
+	/// <summary>
+	///		Sent from client to server when joining the lobby of a server.
+	/// </summary>
+	[Serializable, NetSerializable]
+	public sealed class LobbyJoinedEvent : EntityEventArgs
+	{
+
+	}
+
+	/// <summary>
+	///		Sent from client to server when pressing the "Start Game" button in lobby.
+	/// </summary>
+	[Serializable, NetSerializable]
+	public sealed class StartGamePressedEvent : EntityEventArgs
+	{
+
+	}
+
+	/// <summary>
+	///		Sent from server to client when the game has started.
+	///		Raised locally on server to indicate the game has started.
+	/// </summary>
+	[Serializable, NetSerializable]
+	public sealed class StartGameEvent : EntityEventArgs
+	{
+
+	}
+}
--- Content.Shared/Lobby/SharedLobbySystem.cs
+++ Content.Shared/Lobby/SharedLobbySystem.cs
@@ -1,28 +1,8 @@
 using Robust.Shared.GameObjects;
-using Robust.Shared.Serialization;
-using System;
 
 namespace Content.Shared.Lobby;
 
 public abstract class SharedLobbySystem : EntitySystem
-{
-
-}
-
-[Serializable, NetSerializable]
-public sealed class LobbyJoinedEvent : EntityEventArgs
-{
-
-}
-
-[Serializable, NetSerializable]
-public sealed class StartGamePressedEvent : EntityEventArgs
-{
-
-}
-
-[Serializable, NetSerializable]
-public sealed class StartGameEvent : EntityEventArgs
 {
 
 }
