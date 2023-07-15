df141952dac6e70b16f4d44eae02861346e03465
CL <60152240+collinlunn@users.noreply.github.com>
2023-07-14 23:08:09 -0700

    Adds test wall entity

--- Content.Shared/Movement/CollisionGroup.cs
+++ Content.Shared/Movement/CollisionGroup.cs
@@ -6,19 +6,20 @@ using System;
 
 namespace Content.Shared.Movement
 {
-    /// <summary>
-    ///     Defined collision groups for the physics system.
-    ///     Mask is what it collides with when moving. Layer is what CollisionGroup it is part of.
-    /// </summary>
-    [Flags, PublicAPI]
+	/// <summary>
+	///     Defined collision groups for the physics system.
+	///     Mask is what it collides with when moving. (Bitmask of the layers this component collides with.)
+	///     Layer is what CollisionGroup it is part of. (Bitmask of the collision layers the component is a part of.)
+	/// </summary>
+	[Flags, PublicAPI]
     [FlagsFor(typeof(CollisionLayer)), FlagsFor(typeof(CollisionMask))]
     public enum CollisionGroup
     {
         None = 0,
-        LineOfSight = 1 << 0, // 1, layer for entities that block line of sight
-        Impassable = 1 << 1, // 2, layer for any entities that can never be walked through
-        Mob = 1 << 2, // 4, layer for mobs, players, etc
-        Projectile = 1 << 3, // 8, layer for bullets, etc
+        LineOfSight = 1 << 0, // 1, blocks line of sight
+        Impassable = 1 << 1, // 2, blocks walking
+        Mob = 1 << 2, // 4, mobs, players, etc
+        Projectile = 1 << 3, // 8, bullets, etc
 
         MapGrid = MapGridHelpers.CollisionGroup, // Map grids. This is the actual grid itself, not the walls or other entities connected to the grid.
 
@@ -26,7 +27,14 @@ namespace Content.Shared.Movement
         AllMask = -1,
 
         // Humanoids, etc.
-        MobMask = Impassable | Mob,
+        MobMask = Impassable,
         MobLayer = Mob,
-    }
+
+		// Humanoids, etc.
+		//MobMask = Impassable | HighImpassable | MidImpassable | LowImpassable,
+		//MobLayer = Opaque | BulletImpassable,
+
+		// Airlocks, windoors, firelocks
+		//GlassAirlockLayer = HighImpassable | MidImpassable | BulletImpassable | InteractImpassable,
+	}
 }
