bd82f5e404ed3e081c4c03b9da4d14208641afab
CL <60152240+collinlunn@users.noreply.github.com>
2023-08-22 00:51:50 -0700

    ContentCVars

---
+++ Content.Shared/ContentCVars/ContentCVars.cs
@@ -0,0 +1,12 @@
+﻿using Robust.Shared.Configuration;
+using Robust.Shared;
+
+namespace Content.Shared.ContentCVars
+{
+	[CVarDefs]
+	public sealed class ContentCVars : CVars
+	{
+		public static readonly CVarDef<bool> HudFpsVisible =
+			CVarDef.Create("hud.fps_visible", true, CVar.CLIENTONLY | CVar.ARCHIVE);
+	}
+}
